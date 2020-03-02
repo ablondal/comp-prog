a47479a00f44ee13564608b864cd276c316b9e43
Ari Blondal <ari.blondal@gmail.com>
2020-03-02 15:31:33 -0800

    C done finally

--- Practice/2020_Practice_3/C2.cpp
+++ Practice/2020_Practice_3/C2.cpp
@@ -5,7 +5,8 @@
 
 using namespace std;
 
-// Trying C again but with depth-limited DFS
+// Trying C again but with BFS
+// Done, this time it worked
 
 int main(){
 	int n;
@@ -17,7 +18,7 @@ int main(){
 		file_indices[file_names[i]] = i;
 	}
 
-	vector <int> imps[n];
+	vector <int> G[n];
 
 	int k;
 	string tmp, tmp2;
@@ -33,58 +34,65 @@ int main(){
 				}else{
 					tmp2 = tmp;
 				}
-				imps[i].push_back(file_indices[tmp2]);
+				G[i].push_back(file_indices[tmp2]);
 			}while( tmp[tmp.size()-1] == ',' );
 		}
 	}
 
 	// Now, detect cycles, and store the first we get
 
-	vector <int> cyc;
+	vector <int> cyc(501,0);
+	vector <int> tmpcyc;
 	vector <bool> V(n, false);
+	vector <int> parent(n);
+	int Q[1001];
+	int qa, qb; // qa is the top element
+	// qb is the index of the next empty space
 
-	for(int M_dep = 1; M_dep <= n && cyc.size()==0; ++M_dep ){
+	for(int R=0; R<n; ++R){
+		qa = qb = 0;
+		Q[qb++] = R;
+		V.clear();
+		V.resize(n, false);
+		V[R] = true;
+		parent[R] = -1;
+		tmpcyc.clear();
 
-		for(int i=0; i<n && cyc.size()==0; ++i){
-			V.clear();
-			V.resize(n, false);
-			V[i] = true;
-			int S[n+2];
-			int I[n+2];
-			int ss=0;
-
-			S[ss] = i;
-			I[ss] = 0;
-			while(ss>-1){
-				int C = S[ss];
-				if ( imps[C].size() == I[ss] ){
-					V[C] = false;
-					ss--;
-				}else{
-					S[ss+1] = imps[C][I[ss]];
-					I[ss]++;
-					I[ss+1] = 0;
-					ss++;
-					if (S[0]==S[ss]){
-						for(int i=0; i<ss; ++i){
-							cyc.push_back(S[i]);
-						}
-						break;
-					}
-					if (ss==M_dep){
-						ss--;
-					}
-					V[S[ss]] = true;
+		while(qa!=qb){
+			int C = Q[qa++];
+			for(int i=0; i<G[C].size(); ++i){
+				if ( !V[ G[C][i] ] ){
+					// If not already visited
+					parent[ G[C][i] ] = C;
+					V[ G[C][i] ] = true;
+					Q[qb++] = G[C][i];
+				}else if ( G[C][i] == R ){
+					parent[R] = C;
+					qa = qb;
+					break;
 				}
 			}
-
-
 		}
+		// cout << parent[R] << endl;
+		if (parent[R] != -1){
+			int P = parent[R];
+			while(P!=R){
+				tmpcyc.push_back(P);
+				P = parent[P];
+			}
+			tmpcyc.push_back(R);
+			if (tmpcyc.size() < cyc.size()){
+				// cout << tmpcyc.size() << " is a size" << endl;
+				cyc.clear();
+				cyc.assign(tmpcyc.begin(), tmpcyc.end());
+			}
+		}
+
 	}
 
-	if (cyc.size() != 0){
-		for(int i=0; i<cyc.size(); ++i){
-			if (i!=0) cout << " ";
+	if (cyc.size() != 501){
+		for(int i=cyc.size()-1; i>=0; --i){
+			if (i!=cyc.size()-1) cout << " ";
 			cout << file_names[cyc[i]];
 		}
 		cout << endl;
